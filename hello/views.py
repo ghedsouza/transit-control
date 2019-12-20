e273eced82e115f79c48e8e51d013654090a7f19
Edward D'Souza <edsouza+gh@gmail.com>
2019-12-20 13:26:10 -0500

    WIP?

--- hello/views.py
+++ hello/views.py
@@ -2,17 +2,30 @@ import logging
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Greeting
+from .auth import authenticate_github_webhook_request
 
-# Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-# Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
 
+import functools
+
+def requires_github_hmac(view_func):
+
+    @functools.wraps(view_func)
+    def wrapper(request):
+        authenticate_github_webhook_request(request)
+        return view_func(request)
+
+    return wrapper
+
+
+@csrf_exempt
+@requires_github_hmac
 def github(request):
     if request.POST:
         logger.warning(f"xxx github POST: {request.POST}.")
